2bcecfdd335ac9fc9eda7e7f3a640804401b98ab
Fekade Senbeta <fekadesenbeta@gmail.com>
2020-01-28 13:05:33 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+
+    attr_accessor :name, :artist
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @artist = artist
+        @@all << self 
+    end
+
+
+end
